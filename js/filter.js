b75eed62e75fc7ad42fb484beee8b624634c7dd7
MohammedKhan <mohammedk20259@gmail.com>
2025-12-19 01:16:58 -0500

    fixed the front end, the category filter, and min and max price

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,22 @@
-
-
-
 function loadCategories(categories)
 {
-    const select = document.getElementById('category-select');
+    const select = document.getElementById("category-select");
 
-    categories.forEach(c => {
-        const option = document.createElement('option');
-        option.setAttribute('value', c.categoryId);
+ 
+    select.innerHTML = "";
+
+    // Add default option
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "0";
+    defaultOption.innerText = "Show All";
+    select.appendChild(defaultOption);
+
+    // Add each category once
+    categories.forEach(c =>
+    {
+        const option = document.createElement("option");
+        option.value = c.categoryId;
         option.innerText = c.name;
         select.appendChild(option);
-    })
+    });
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-})
